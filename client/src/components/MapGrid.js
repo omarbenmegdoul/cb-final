baf3775dcb32bd125431191a09f42a42f44e57df
Omar Benmegdoul <omarbenmeg@gmail.com>
2021-07-06 22:38:55 -0400

    right-click to erase map selection

--- client/src/components/MapGrid.js
+++ client/src/components/MapGrid.js
@@ -1,40 +1,12 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import {
-  CHI_SUBDIVISIONS,
-  PSI_SUBDIVISIONS
-} from '../constants';
+import { CHI_SUBDIVISIONS, PSI_SUBDIVISIONS } from '../constants';
 import { subdivisionCenterFromChiPsiCoords } from '../utils';
 import SubdivisionContext from './Context/SubdivisionsContext';
 
-
-let allowStaging = false;
-const beginStaging = (ev) => {
-    ev.preventDefault();
-
-    console.log('Begin staging');
-    ev.target.classList.add('selected');
-    allowStaging = true;
-};
-
-const markHover = (ev) => {
-    ev.preventDefault();
-    if (!allowStaging) {
-        return;
-    }
-    ev.target.classList.add('selected'); //TODO-mid: right mouse button for erase
-    logElementCoords(ev);
-};
-
-const logElementCoords = (ev) => {
-    ev.preventDefault();
-    console.log(ev.target.id);
-};
-
 const MapGrid = ({ Props }) => {
-    const {setSelectedSubdivisions,
-    subdivisionData, setSubdivisionData
-     } = React.useContext(SubdivisionContext)
+    const { setSelectedSubdivisions, subdivisionData, setSubdivisionData } =
+        React.useContext(SubdivisionContext);
     const rows = new Array(PSI_SUBDIVISIONS).fill(0);
     const columns = new Array(CHI_SUBDIVISIONS).fill(0);
     // code to populate DB
@@ -69,27 +41,59 @@ const MapGrid = ({ Props }) => {
     //   };
     //   fetch("/dict",options);
     // }, []);
+
     React.useEffect(() => {
         const fetcher = async () => {
             const res = await fetch('http://localhost:5678/subdivisions');
             const json = await res.json();
             const data = await JSON.parse(json).data;
-            setSubdivisionData(data.reduce((accum,x)=>{
-              const addition= {[x._id]:x.listings}
-              return {...accum,...addition}
-            },{}))
+            setSubdivisionData(
+                data.reduce((accum, x) => {
+                    const addition = { [x._id]: x.listings };
+                    return { ...accum, ...addition };
+                }, {})
+            );
             return;
         };
         fetcher();
     }, []);
 
+    const [paintingState, setPaintingState] = React.useState({
+        allowStaging: false,
+        mouseButton: null,
+    });
+    const handlePaintingClassChange = (element, paintMode) => {
+        const callClassListMethod = paintMode
+            ? () => {
+                  element.classList.remove('selected');
+              }
+            : () => {
+                  element.classList.add('selected');
+              };
+        callClassListMethod();
+    };
+    const beginStaging = (ev) => {
+        ev.preventDefault();
+        console.log(ev);
+        console.log('Begin staging');
+        setPaintingState({ allowStaging: true, paintMode: ev.button });
+        handlePaintingClassChange(ev.target, ev.button);
+    };
+    const markHover = (ev) => {
+        ev.preventDefault();
+        if (!paintingState.allowStaging) {
+            return;
+        }
+        handlePaintingClassChange(ev.target, paintingState.paintMode);
+        logElementCoords(ev);
+    };
     const stopStaging = (ev) => {
         ev.preventDefault();
-        if (!allowStaging) {
+        if (!paintingState.allowStaging) {
             return;
         }
         console.log('commit');
-        allowStaging = false;
+        setPaintingState({ allowStaging: false, paintMode: null });
         const SubDs = document.getElementById('map-grid-wrapper').childNodes;
         const toggledSubDs = [...SubDs]
             .filter((node) => {
@@ -100,7 +104,11 @@ const MapGrid = ({ Props }) => {
             });
         setSelectedSubdivisions(toggledSubDs);
     };
-    
+    const logElementCoords = (ev) => {
+        ev.preventDefault();
+        console.log(ev.target.id);
+    };
+
     return (
         <TestFlex>
             <Wrapper
@@ -111,14 +119,17 @@ const MapGrid = ({ Props }) => {
             >
                 {rows.map((x, xIndex) => {
                     return columns.map((y, yIndex) => {
-                      
                         return (
                             <div
                                 id={`${xIndex}-${yIndex}`}
                                 className="grid-slots"
                                 onMouseEnter={markHover}
+                                onContextMenu={(ev)=>{ev.preventDefault();return false}}
                             >
-                                {!!subdivisionData[`${xIndex}-${yIndex}`]?.length && subdivisionData[`${xIndex}-${yIndex}`]?.length}
+                                {!!subdivisionData[`${xIndex}-${yIndex}`]
+                                    ?.length &&
+                                    subdivisionData[`${xIndex}-${yIndex}`]
+                                        ?.length}
                             </div>
                         );
                     });
@@ -154,23 +165,22 @@ const Wrapper = styled.div`
     justify-content: center;
     align-items: center;
     z-index: 3;
-    
 
     & .grid-slots {
         width: calc(100% / ${CHI_SUBDIVISIONS});
         height: calc(100% / ${PSI_SUBDIVISIONS});
         border-radius: 2px;
-        display:flex;
-        flex-direction:column;
-        justify-content:center;
-        align-items:center;
-        color:rgba(0,0,0,0);
-        font-size:10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: rgba(0, 0, 0, 0);
+        font-size: 10px;
     }
 
     & .grid-slots:hover {
         border: 2px #f0f dotted;
-        color:rgba(0,0,0,1)
+        color: rgba(0, 0, 0, 1);
     }
 
     & .selected {
